8652c56017f05fda48029e773208b6403490d634
Jucelio Delmondes <jucelio.2013@hotmail.com>
2020-09-30 19:25:01 -0300

    alteracao dir

--- src/main/java/com/access/controller/service/ImageService.java
+++ src/main/java/com/access/controller/service/ImageService.java
@@ -8,8 +8,6 @@ package com.access.controller.service;
 import com.access.controller.models.UserModel;
 import com.access.controller.requestObject.RegisterUserRequestObject;
 import java.io.FileOutputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +19,7 @@ import org.springframework.stereotype.Service;
 public class ImageService {
     
     public void saveImage(RegisterUserRequestObject information, UserModel user) {
-        new Thread(saveImageRunnable(information, user)).run();
+        new Thread(saveImageRunnable(information, user)).start();
     }
     
     private Runnable saveImageRunnable(RegisterUserRequestObject information, UserModel user) {
@@ -29,14 +27,12 @@ public class ImageService {
             String[] b64 = information.getImage().split(",");
             byte[] result = Base64.decodeBase64(b64[1]);
             try {
-                FileOutputStream stream = new FileOutputStream("src\\main\\resources\\static\\"+user.getToken()+".png");
+                FileOutputStream stream = new FileOutputStream("src\\main\\resources\\static\\images\\"+user.getToken()+".png");
                 stream.write(result);
                 stream.close();
                 System.out.println("Salvando imagem para o usuario: "+user.getName());
             } catch (Exception er) {
                 er.printStackTrace();
-            } catch (Throwable ex) {
-                Logger.getLogger(ImageService.class.getName()).log(Level.SEVERE, null, ex);
             }
         };
     }
